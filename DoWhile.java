dcf23000837c9fe37fece3743189a937c1f7fe8c
Shaik Abdul Malik <shaikabdulmalik185@gmail.com>
2025-07-15 06:13:03 +0530

    we learn Do-While loop

--- DoWhile.java
+++ DoWhile.java
@@ -1,6 +1,10 @@
 public class DoWhile {
     public static void main(String args[])
     {
-        
+        int i = 6;
+        do {
+            System.out.println("Shaik " + i);
+            i++;
+        } while (i <= 5);
     }
 }
