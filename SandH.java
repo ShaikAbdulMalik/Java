376b152e97fee14970500dda6387a28d94df9726
Shaik Abdul Malik <shaikabdulmalik185@gmail.com>
2025-07-23 01:26:13 +0530

    Learned about Stack Memory and Heap memory in java

---
+++ SandH.java
@@ -0,0 +1,28 @@
+class Calculator 
+{
+    int num = 10;
+
+    public int add (int n1, int n2)
+    {
+        return n1 + n2;
+    }
+}
+
+public class SandH {
+  public static void main(String[] args) {
+    
+    int data = 10;
+
+    Calculator obj = new Calculator();
+    Calculator obj1 = new Calculator();
+
+    int r1 = obj.add(2, 3);
+    // System.out.println(r1);
+
+    obj.num = 12; // if i do this on;y the obj value is changed but not the other one since only obj is impacted thats why
+    System.out.println(obj.num);
+    System.out.println(obj1.num);
+
+    
+  }  
+}
