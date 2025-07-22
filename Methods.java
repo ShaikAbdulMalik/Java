2213e291c728ab27f94e42b50a7bd554e57e4961
Shaik Abdul Malik <shaikabdulmalik185@gmail.com>
2025-07-22 06:11:34 +0530

    learned about methods in Java

---
+++ Methods.java
@@ -0,0 +1,26 @@
+class Computer
+{
+    public void playingMusic()
+    {
+        System.out.println("playing music...");
+    }
+
+    public String ConnectMouse (int port)
+    {
+        if(port <= 2)
+            return "Mouse Connected...";
+        else
+            return "Wrong port";
+        
+    }
+}
+
+public class Methods {
+    public static void main(String[] args) {
+     
+        Computer obj = new Computer();
+        obj.playingMusic();
+        String str =obj.ConnectMouse(3);
+        System.out.println(str);
+    }
+}
