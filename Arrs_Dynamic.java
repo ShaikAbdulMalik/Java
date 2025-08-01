133955d99ba678b08602485251e3fd5004dab909
Shaik Abdul Malik <shaikabdulmalik185@gmail.com>
2025-08-01 11:03:21 +0530

    learned about arrays

---
+++ Arrs_Dynamic.java
@@ -0,0 +1,19 @@
+public class Arrs_Dynamic {
+    public static void main (String args[])
+    {
+        int nums[] = new int[4];
+
+        nums[0] = 3;
+        nums[1] = 1;
+        nums[2] = 2;
+        nums[3] = 8;
+
+        
+        for(int i = 0; i<4; i++)
+        {
+            System.out.println(nums[i]);
+
+        }
+    }
+    
+}
---
+++ arrs.java
@@ -0,0 +1,24 @@
+public class arrs {
+    public static void main(String[] args) {
+        
+        int nums[] = {1,2,3,4,5};
+        System.out.println("Before Update");
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        System.out.println(nums[3]);
+        System.out.println(nums[4]);
+    //Hence according to the indexes the output is '1' and so on..
+    
+    nums[2] = 6;
+        System.out.println("After update");
+        System.out.println(nums[0]);
+        System.out.println(nums[1]);
+        System.out.println(nums[2]);
+        System.out.println(nums[3]);
+        System.out.println(nums[4]);
+
+
+
+    }
+}
