7f9f3f8e9d4875fd629bf95ddf4e33f96eb3c8ca
Shaik Abdul Malik <shaikabdulmalik185@gmail.com>
2025-08-15 17:41:28 +0530

    learned about strings!!

---
+++ string_str.java
@@ -0,0 +1,12 @@
+public class string_str {
+    public static void main(String[] args) {
+        
+        String name = new String("Malik");
+
+        System.out.println("Hello " + name);
+        System.out.println(name.hashCode());
+        System.out.println(name.charAt(2));
+        System.out.println(name.toLowerCase());
+        System.out.println(name.toUpperCase());
+    }
+}
