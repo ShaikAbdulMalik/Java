fa5d444790934e39cee49bd9be89d977336ff029
Shaik Abdul Malik <shaikabdulmalik185@gmail.com>
2025-08-16 12:05:49 +0530

    learned about static variables

---
+++ static_var.java
@@ -0,0 +1,26 @@
+class Car
+{
+    String lamborghini;
+    String ferrari;
+    static int horsepower;
+    public void show()
+    {
+        System.out.println(ferrari + " : " + horsepower + " : " + lamborghini );
+    }
+}
+public class static_var {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.lamborghini = "black";
+        Car.horsepower = 1200; // The static variable should be accessed in a static way i.e by using its class name 
+        
+        Car car2 = new Car();
+        car2.ferrari = "red";
+        Car.horsepower = 1200;
+
+        Car.horsepower =2200;
+
+        car1.show();
+        car2.show();
+    }
+}
