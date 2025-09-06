4f48df73b02b6be412bfa1812878ecdce0ad4622
ShaikAbdulMalik <shaikabdulmalik185@gmail.com>
2025-09-06 07:05:45 +0530

    Learned aabout encapsulation in java

---
+++ ENCAP_oops.java
@@ -0,0 +1,21 @@
+class student
+{
+    private String Name;
+    public String getName ()
+    {
+        return Name;
+    }
+    public String setName(String NewName)
+    {   
+        Name = NewName;
+        return NewName;
+    }
+}
+public class ENCAP_oops {
+    public static void main(String[] args) {
+        student s1 = new student();
+        s1.setName("Abdul Malik");
+        System.out.println("Student name : " + s1.getName());
+
+    }
+}
