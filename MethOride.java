96e8d2cb8bcb731b6ac0867c63a2982cd03349b7
ShaikAbdulMalik <shaikabdulmalik185@gmail.com>
2025-09-08 13:20:32 +0530

    learned about method overriding

---
+++ MethOride.java
@@ -0,0 +1,23 @@
+class Calc
+{
+    public int add(int n1, int n2)
+    {
+        return n1+n2; //here we gget 30 as the answer
+    }
+}
+class Advcalc extends Calc
+{
+    //Now if i comments this whole statment out then java will go to the parent class and ask if it has the add function (ex: if n1 = 2 and n2 = 3 then according to Calc we'll get 5 as the answer)
+    
+    public int add(int n1, int n2)  //therefore this add method overrides the add method in Calc class
+    {
+        return n1+n2+1; //here we get 31 as the anwer 
+    }
+}
+public class MethOride {
+    public static void main(String[] args) {
+        Advcalc a = new Advcalc();
+        int r1 = a.add(10, 20);
+        System.out.println(r1);
+    }
+}
