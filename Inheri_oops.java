51af60b3c253a3ba3bcde66412a8743fd2599c7d
ShaikAbdulMalik <shaikabdulmalik185@gmail.com>
2025-09-08 07:49:43 +0530

    learned about multilevel inheritance

--- Inheri_oops.java
+++ Inheri_oops.java
@@ -1,14 +1,16 @@
 public class Inheri_oops {
     public static void main(String[] args) {
         
-        Inheri_AdvCalc obj = new Inheri_AdvCalc();
+        Inheri_VadvCalc obj = new Inheri_VadvCalc();
         
         int r1 = obj.add(10, 20);
         int r2 = obj.sub(30, 20);
         int r3 = obj.multi(3, 2);
         int r4 = obj.div(30, 5);
+        double r5 = obj.power(2,5);
 
         System.out.println("Add and sub : " + r1 + " and " + r2);  
-        System.out.println("Multi and div : " + r3 + " and " + r4);  
+        System.out.println("Multi and div : " + r3 + " and " + r4); 
+        System.out.println("powers are : " + r5); 
     }
 }
