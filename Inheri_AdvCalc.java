c69a486a7ba3e933e7957e98ff819110f1658972
ShaikAbdulMalik <shaikabdulmalik185@gmail.com>
2025-09-08 07:23:16 +0530

    learned about inheritancee

---
+++ Inheri_AdvCalc.java
@@ -0,0 +1,11 @@
+public class Inheri_AdvCalc extends Inheri_Calc
+{
+    public int multi(int n1, int n2)
+    {
+        return(n1*n2);
+    }
+    public int div(int n1, int n2)
+    {
+        return(n1/n2);
+    }
+}
---
+++ Inheri_oops.java
@@ -0,0 +1,14 @@
+public class Inheri_oops {
+    public static void main(String[] args) {
+        
+        Inheri_AdvCalc obj = new Inheri_AdvCalc();
+        
+        int r1 = obj.add(10, 20);
+        int r2 = obj.sub(30, 20);
+        int r3 = obj.multi(3, 2);
+        int r4 = obj.div(30, 5);
+
+        System.out.println("Add and sub : " + r1 + " and " + r2);  
+        System.out.println("Multi and div : " + r3 + " and " + r4);  
+    }
+}
